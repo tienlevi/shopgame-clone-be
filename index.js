325afbc290c9133fc6cef656e2d322e0855ab5dc
TiennLevii <nguyentrachtien2401@gmail.com>
2024-03-13 09:39:59 +0700

    commit again

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://shopgame-clone.vercel.app" || "http://localhost:3000",
+    origin: "https://shopgame-clone.vercel.app" && "http://localhost:3000",
     methods: ["GET", "POST", "OPTIONS"],
     credentials: true,
   })
@@ -23,7 +23,7 @@ app.use(
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://shopgame-clone.vercel.app"
+    "https://shopgame-clone.vercel.app" && "http://localhost:3000"
   );
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
