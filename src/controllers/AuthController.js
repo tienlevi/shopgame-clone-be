6814ae11c8e9072d334394b7abd35019c8f3d285
TiennLevii <nguyentrachtien2401@gmail.com>
2024-03-14 15:30:03 +0700

    fix auth

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -23,18 +23,16 @@ export const RefreshToken = async (req, res) => {
 };
 
 export const AccessToken = async (req, res) => {
-  app.get("/user", AuthenticateToken, async (req, res) => {
-    const { username } = req.user;
+  const { username } = req.user;
 
-    try {
-      const user = await UserModel.findOne({ username });
-      if (!user) {
-        return res.status(403).json({ error: "User not found" });
-      }
-      return res.status(200).json({ user });
-    } catch (error) {
-      console.error(error);
-      return res.status(401).json({ error: "Unauthorized" });
+  try {
+    const user = await UserModel.findOne({ username });
+    if (!user) {
+      return res.status(403).json({ error: "User not found" });
     }
-  });
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ error: "Unauthorized" });
+  }
 };
--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,27 @@
 import express from "express";
-import { AccessToken, RefreshToken } from "../controllers/AuthController.js";
+import UserModel from "../model/User.js";
+import { RefreshToken } from "../controllers/AuthController.js";
 import { Login, Register } from "../controllers/FormController.js";
+import AuthenticateToken from "../controllers/AuthenticateToken.js";
 
 const router = express.Router();
 
 router.post("/refresh", RefreshToken);
 
 router.post("/login", Login);
-router.post("/user", AccessToken);
+router.get("/user", AuthenticateToken, async (req, res) => {
+  const { username } = req.user;
+
+  try {
+    const user = await UserModel.findOne({ username });
+    if (!user) {
+      return res.status(403).json({ error: "User not found" });
+    }
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.error(error);
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+});
 router.post("/signup", Register);
 export default router;
